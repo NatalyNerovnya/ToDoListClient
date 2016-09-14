b29e918ed2c0216679083c893b0c7da4dc85bcf6
KoroliovaNastia <K.Nastia.san@mail.ru>
2016-09-14 13:49:56 +0300

    bootstrap 2ch

--- ToDoListClient/Scripts/todo.js
+++ ToDoListClient/Scripts/todo.js
@@ -7,7 +7,7 @@
         var tr = $("<tr data-id='" + obj.ToDoLocalId + "'></tr>");
         tr.append("<td><input type='checkbox' class='completed' " + (obj.IsCompleted ? "checked" : "") + "/></td>");
         tr.append("<td class='name' >" + obj.Name + "</td>");
-        tr.append("<td><input type='button' class='delete-button' value='Delete' /></td>");
+        tr.append("<td><input type='button' class='delete-button btn-warning' value='Delete' /></td>");
         $(parentSelector).append(tr);
     }
 
